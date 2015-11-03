2aa108a5dea17ca7ec9847726025157b8e515dd5
Javithalion <javier_cervera_cano@hotmail.com>
2015-11-03 11:12:15 +0100

    Initial commit

---
+++ WatchDogService/Model/StatusWatcher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatchDogService.Model
+{
+    public abstract class StatusWatcher
+    {
+        public Guid Identifier { get; protected set; }
+        public string Name { get; protected set; }
+        public DateTime CreationDate { get; protected set; }
+        public DateTime LastCheck { get; protected set; }
+        public TimeSpan RefreshPeriod { get; protected set; }
+
+
+
+    }
+}
---
+++ WatchDogService/Services/Implementations/ConfigurationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WatchDogService.Services.Interfaces;
+
+namespace WatchDogService.Services.Implementations
+{
+    public class ConfigurationService : IConfigurationService
+    {
+    }
+}
